5024bc6b7fb89f4a9c768a8bda3883386d4c5eb6
Abhinav Tiwari <abhinavtiwari1729@gmail.com>
2023-07-09 13:57:22 +0530

    changes for ternary operator

--- Abhinav_01_Fundamentals-part-1/script.js
+++ Abhinav_01_Fundamentals-part-1/script.js
@@ -1,2 +1,46 @@
-let js = 'abhinav';
-//console.log(10*10*10 + 9*9*9);
+// let country = 'India';
+// let continent = 'Asia';
+// let population = '1400 million';
+
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+// let isIsland = false;
+// let language;
+// console.log(typeof(isIsland));
+// console.log(typeof(country));
+// console.log(typeof(continent));
+// console.log(typeof(population));
+// console.log(typeof(language));
+
+
+// let massMark = 78;
+// let heightMark = 1.69;
+// let massJohn = 92 , heightJohn = 1.95 ;
+
+// let BMIMark = massMark / (heightMark * heightMark) ;
+
+// let BMIJohn = massJohn / (heightJohn * heightJohn) ;
+
+// console.log(BMIMark);
+// console.log(BMIJohn);
+
+// let markHigherBMI = BMIMark > BMIJohn ;
+
+// console.log(markHigherBMI);
+
+// console.log(`this is 
+// my first template literal
+// abhinav`) ;
+
+// let webNumber = prompt('Please enter a number');
+// console.log(webNumber);
+
+const bill = 275;
+
+/* Write your code below. Good luck! 🙂 */
+
+let tip = bill > 50 && bill < 300 ? bill*15/100 : bill*20/100 ;
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
