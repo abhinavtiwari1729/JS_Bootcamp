8b8194749d12b445af64f18407c9d6085938ca24
Abhinav Tiwari <abhinavtiwari1729@gmail.com>
2023-07-11 01:07:18 +0530

    assignment_11thjuly

--- Abhinav_01_Fundamentals-part-1/script.js
+++ Abhinav_01_Fundamentals-part-1/script.js
@@ -1,46 +1,68 @@
-// let country = 'India';
-// let continent = 'Asia';
-// let population = '1400 million';
+let country = 'India';
+let continent = 'Asia';
+let population = '1400 million';
 
-// console.log(country);
-// console.log(continent);
-// console.log(population);
+console.log(country);
+console.log(continent);
+console.log(population);
 
-// let isIsland = false;
-// let language;
-// console.log(typeof(isIsland));
-// console.log(typeof(country));
-// console.log(typeof(continent));
-// console.log(typeof(population));
-// console.log(typeof(language));
+let isIsland = false;
+let language;
+console.log(typeof(isIsland));
+console.log(typeof(country));
+console.log(typeof(continent));
+console.log(typeof(population));
+console.log(typeof(language));
 
 
-// let massMark = 78;
-// let heightMark = 1.69;
-// let massJohn = 92 , heightJohn = 1.95 ;
+let massMark = 78;
+let heightMark = 1.69;
+let massJohn = 92 , heightJohn = 1.95 ;
 
-// let BMIMark = massMark / (heightMark * heightMark) ;
+let BMIMark = massMark / (heightMark * heightMark) ;
 
-// let BMIJohn = massJohn / (heightJohn * heightJohn) ;
+let BMIJohn = massJohn / (heightJohn * heightJohn) ;
 
-// console.log(BMIMark);
-// console.log(BMIJohn);
+console.log(BMIMark);
+console.log(BMIJohn);
 
-// let markHigherBMI = BMIMark > BMIJohn ;
+let markHigherBMI = BMIMark > BMIJohn ;
 
-// console.log(markHigherBMI);
+console.log(markHigherBMI);
 
-// console.log(`this is 
-// my first template literal
-// abhinav`) ;
+console.log(`this is 
+my first template literal
+abhinav`) ;
 
 // let webNumber = prompt('Please enter a number');
 // console.log(webNumber);
 
 const bill = 275;
 
+//let bill = Number(prompt("enter bill amount"));
 /* Write your code below. Good luck! 🙂 */
 
 let tip = bill > 50 && bill < 300 ? bill*15/100 : bill*20/100 ;
 
 console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
+
+let calcAverage = (value1 , value2 , value3) => (value1 + value2 + value3)/3 ;
+
+
+
+let scoreDolphins = calcAverage(44 , 23 , 71) ;
+let scoreKoalas = calcAverage(65 , 54 , 49) ;
+
+
+let checkWinner = function(avgDolphins , avgKoalas) {
+    
+    if(avgDolphins > 2*avgKoalas)
+        console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+    else if (avgKoalas > 2*avgDolphins)
+        console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+    else
+        console.log(`No team wins...`);
+    
+}
+
+checkWinner(scoreDolphins , scoreKoalas);
